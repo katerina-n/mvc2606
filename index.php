b54fedc2f2faf148001611e9dd331b5aae90e5a0
Andrii Popov <andrew.p@codesushi.co>
2017-09-22 16:27:38 +0000

    front controller setup 1

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo 'MVC init';
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', __DIR__ . DS);
+
+spl_autoload_register(function($className) {
+    $path = ROOT . str_replace('\\', DS, $className) . '.php';
+    
+    if (!file_exists($path)) {
+        throw new \Exception("{$path} not found");
+    }
+    
+    require $path;
+});
+
+try {
+    $request = new \Framework\Request($_GET, $_POST);
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+
+
+require 'layout.phtml';
