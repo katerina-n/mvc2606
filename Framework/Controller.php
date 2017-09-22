a0df815565fbcbc31620dc6275b7314e8ae9efaf
Andrii Popov <andrew.p@codesushi.co>
2017-09-22 17:54:30 +0000

    reponse setup

--- Framework/Controller.php
+++ Framework/Controller.php
@@ -9,13 +9,20 @@ abstract class Controller
         extract($args);
         
         $path = str_replace(['Controller', '\\'], '', get_class($this));
+        $file = VIEW_DIR . $path . DS . $view;
+        
+        if (!file_exists($file)) {
+            throw new \Exception("{$file} not found");
+        }
+        
         ob_start();
-        require VIEW_DIR . $path . DS . $view;
+        require $file;
         $content = ob_get_clean();
         
         ob_start();
         require VIEW_DIR . 'layout.phtml';
+        $html = ob_get_clean();
         
-        return ob_get_clean();
+        return new Response($html);
     }
 }
