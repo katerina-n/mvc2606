e5023fd2e185a41ab6083638f047528ddd8fad07
Andrii Popov <andrew.p@codesushi.co>
2017-10-07 07:44:08 +0000

    security controller and views

---
+++ Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        
+    }
+    
+    public function logoutAction()
+    {
+        
+    }
+    
+    public function changePasswordAction()
+    {
+        
+    }
+    
+    public function registerAction()
+    {
+        
+    }
+    
+    public function activateAccountAction()
+    {
+        
+    }
+}
