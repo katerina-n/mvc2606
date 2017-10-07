3a8789a040edb0e26d9ea74f67da69e9edd7f406
Andrii Popov <andrew.p@codesushi.co>
2017-10-07 11:22:16 +0000

    render login form

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -3,12 +3,25 @@
 namespace Controller;
 
 use Framework\Controller;
+use Framework\Request;
+use Model\Form\LoginForm;
 
 class SecurityController extends Controller
 {
-    public function loginAction()
+    public function loginAction(Request $request)
     {
+        $form = new LoginForm(
+            $request->post('email'), 
+            $request->post('password')
+        );
         
+        if ($request->isPost()) {
+            if ($form->isValid()) {
+                
+            }
+        }
+        
+        return $this->render('login.phtml', ['form' => $form]);
     }
     
     public function logoutAction()
